f7f76332637f1e2fd2a4369c500fb6846159c773
Edu <Edu@Edus-Mac.local>
2017-05-10 22:05:41 +0200

    first commit

---
+++ FantasticEdu.swift
@@ -0,0 +1,16 @@
+//
+//  FantasticEdu.swift
+//  FantasticEdu
+//
+//  Created by Edu on 10/05/2017.
+//  Copyright © 2017 Edu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FantasticEdu: UIResponder {
+    
+    var test = "Test"
+
+}
